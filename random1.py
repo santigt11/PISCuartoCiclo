c2785c7f4828f3acc648fe2a0f46e6da9e4f0bc2
Steven Luna <steven.luna@unl.edu.ec>
2024-07-28 21:04:00 -0500

    Random1 Commit

--- random1.py
+++ random1.py
@@ -1,11 +1,11 @@
-import random1
+import random as random1
 def generar_numero_aleatorio(minimo=30, maximo=30):
 
     if minimo > maximo:
         raise ValueError("El valor mínimo debe ser menor o igual al valor máximo")
-    return random.randint(minimo, maximo)
+    return random1.randint(minimo, maximo)
 
 def generar_numero_aleatorio_Egresados(minimo=3, maximo=7):
     if minimo > maximo:
         raise ValueError("El valor mínimo debe ser menor o igual al valor máximo")
-    return random.randint(minimo, maximo)
+    return random1.randint(minimo, maximo)
